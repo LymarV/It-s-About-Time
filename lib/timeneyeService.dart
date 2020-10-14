a2697a04119fdb30987b9e463f51a68f9fa54559
Vladimir Lymar <lymar.vm@gmail.com>
2020-10-14 13:03:53 +0300

    Add the ability to set and store the API key in the Timeneye service

--- lib/timeneyeService.dart
+++ lib/timeneyeService.dart
@@ -5,10 +5,24 @@ import 'package:its_about_time/Timeneye/timer.dart';
 
 import 'Timeneye/timers.dart';
 
+import 'package:shared_preferences/shared_preferences.dart';
+
 class TimeneyeService {
   String _apiKey = '';
 
   List<Timer> timers;
+
+  TimeneyeService() {
+    _loadApiKey();
+  }
+
+  void SetApiKey(String apiKey) async {
+    _apiKey = apiKey;
+
+    var prefs = await SharedPreferences.getInstance();
+
+    await prefs.setString('apiKey', _apiKey);
+  }
 
   void loadTimers() async {
     String dataURL = "https://track.timeneye.com/api/3/timers";
@@ -23,5 +37,11 @@ class TimeneyeService {
     if (timersObject.timers.isEmpty) return;
 
     timers = timersObject.timers;
+  }
+
+  void _loadApiKey() async {
+    var prefs = await SharedPreferences.getInstance();
+
+    _apiKey = prefs.getString('apiKey');
   }
 }
