2b3c5192bf72562eb8bd1ed46bd3f73d9d494929
Vladimir Lymar <lymar.vm@gmail.com>
2020-10-23 11:04:03 +0300

    Implement entries loading in the time page

--- lib/timePage.dart
+++ lib/timePage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/semantics.dart';
+import 'package:its_about_time/Time/timeEntry.dart';
+import 'package:its_about_time/Time/timeService.dart';
 
 class TimePage extends StatefulWidget {
   @override
@@ -10,22 +12,42 @@ class TimePage extends StatefulWidget {
 }
 
 class _TimePageState extends State<TimePage> {
+  final TimeService timeService = TimeService.getInstance();
+
+  List<TimeEntry> entries = List.empty();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _loadEntries();
+
     return CupertinoTabView(
       builder: (context) {
         return CupertinoPageScaffold(
-          navigationBar: CupertinoNavigationBar(
-            middle: Text('Time'),
-          ),
-          child: Container(
-              alignment: Alignment.center,
-              color: CupertinoColors.activeOrange,
-              child: Container(
-                child: Text('Time'),
-              )),
-        );
+            navigationBar: CupertinoNavigationBar(
+              middle: Text('Time'),
+            ),
+            child: Container(
+                alignment: Alignment.center,
+                color: CupertinoColors.activeOrange,
+                child: ListView.builder(
+                  itemCount: entries.length,
+                  itemBuilder: (context, position) {
+                    return Text(entries[position].description);
+                  },
+                )));
       },
     );
+  }
+
+  _loadEntries() async {
+    var list = await timeService.loadTimeEntries();
+    setState(() {
+      entries = list;
+    });
   }
 }
