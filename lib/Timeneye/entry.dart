0c3df4dc97d922c654aac247c468ac28246ab3c1
Vladimir Lymar <lymar.vm@gmail.com>
2020-10-23 10:40:27 +0300

    Add date and time properties to the Timeneye models

--- lib/Timeneye/entry.dart
+++ lib/Timeneye/entry.dart
@@ -1,3 +1,4 @@
+import 'package:its_about_time/Time/timeEntry.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'dart:core';
 import 'package:json_annotation/json_annotation.dart';
@@ -6,15 +7,33 @@ part 'entry.g.dart';
 
 @JsonSerializable()
 class Entry {
-  Entry(this.id, this.projectId, this.projectName, this.taskId, this.taskName,
-      this.notes);
+  Entry(this.id, this.entryDate, this.minutes, this.projectId, this.projectName,
+      this.taskId, this.taskName, this.notes) {
+    var date = DateTime.tryParse(entryDate);
+    if (date != null) {
+      _date = date;
+    }
+
+    var time = int.tryParse(minutes);
+    if (time != null) {
+      _time = time;
+    }
+  }
 
   String id;
+  String entryDate;
+  String minutes;
   String projectId;
   String projectName;
   String taskId;
   String taskName;
   String notes;
+
+  DateTime _date;
+  DateTime get date => _date;
+
+  int _time;
+  int get time => _time;
 
   factory Entry.fromJson(Map<String, dynamic> json) => _$EntryFromJson(json);
 
--- lib/Timeneye/timer.dart
+++ lib/Timeneye/timer.dart
@@ -6,15 +6,28 @@ part 'timer.g.dart';
 
 @JsonSerializable()
 class Timer {
-  Timer(this.id, this.projectId, this.projectName, this.taskId, this.taskName,
-      this.notes);
+  Timer(this.id, this.minutes, this.projectId, this.projectName, this.taskId,
+      this.taskName, this.notes) {
+    date = DateTime.now();
+
+    var time = int.tryParse(minutes);
+    if (time != null) {
+      _time = time;
+    }
+  }
+
+  DateTime date;
 
   String id;
+  String minutes;
   String projectId;
   String projectName;
   String taskId;
   String taskName;
   String notes;
+
+  int _time;
+  int get time => _time;
 
   factory Timer.fromJson(Map<String, dynamic> json) => _$TimerFromJson(json);
 
