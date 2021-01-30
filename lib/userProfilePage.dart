e9f87ceab15091168757442ede715d80ee7f1c14
Vladimir Lymar <lymar.vm@gmail.com>
2021-01-30 14:25:19 +0200

    Implement basic Upwork auth logic

--- lib/userProfilePage.dart
+++ lib/userProfilePage.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:its_about_time/Timeneye/timeneyeService.dart';
 
+import 'package:oauth1/oauth1.dart' as oauth1;
+
 class UserProfilePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -19,6 +21,13 @@ class _UserProfilePageState extends State<UserProfilePage> {
   IconData timeneyeKeyVisibilityIcon = CupertinoIcons.eye_slash;
 
   @override
+  void initState() {
+    super.initState();
+
+    _upworkOauth();
+  }
+
+  @override
   Widget build(BuildContext context) {
     _loadApiKey();
 
@@ -28,51 +37,56 @@ class _UserProfilePageState extends State<UserProfilePage> {
           navigationBar: CupertinoNavigationBar(
             middle: Text('Profile'),
           ),
-          child: Container(
-              padding: EdgeInsets.all(50),
-              alignment: Alignment.center,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  SizedBox(height: 100),
-                  Row(
-                    children: [
-                      Text(
-                        'Timeneye API Key',
-                        style: TextStyle(
-                          color: Color(0xff455272),
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Flexible(
-                        child: CupertinoTextField(
-                          obscureText: !timeneyeApiKeyFieldVisible,
-                          controller: apiKeyTextController,
-                          placeholder: 'API key',
-                        ),
-                      ),
-                      CupertinoButton(
-                          child: Icon(
-                            timeneyeKeyVisibilityIcon,
-                            color: Color(0xff455272),
-                          ),
-                          onPressed: _toggleTimeneyeKeyVisibility),
-                    ],
-                  ),
-                  SizedBox(height: 100),
-                  CupertinoButton.filled(
-                    child: Text('Save'),
-                    onPressed: _saveApiKey,
-                  ),
-                ],
-              )),
+          child: _getMainContainer(),
         );
       },
     );
+  }
+
+  // HACK! to enable hot reload inside the CupertiroTabView
+  Widget _getMainContainer() {
+    return Container(
+        padding: EdgeInsets.all(50),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 100),
+            Row(
+              children: [
+                Text(
+                  'Timeneye API Key',
+                  style: TextStyle(
+                    color: Color(0xff455272),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Flexible(
+                  child: CupertinoTextField(
+                    obscureText: !timeneyeApiKeyFieldVisible,
+                    controller: apiKeyTextController,
+                    placeholder: 'API key',
+                  ),
+                ),
+                CupertinoButton(
+                    child: Icon(
+                      timeneyeKeyVisibilityIcon,
+                      color: Color(0xff455272),
+                    ),
+                    onPressed: _toggleTimeneyeKeyVisibility),
+              ],
+            ),
+            SizedBox(height: 100),
+            CupertinoButton.filled(
+              child: Text('Save'),
+              onPressed: _saveApiKey,
+            ),
+          ],
+        ));
   }
 
   void _saveApiKey() {
@@ -93,5 +107,47 @@ class _UserProfilePageState extends State<UserProfilePage> {
           ? CupertinoIcons.eye
           : CupertinoIcons.eye_slash;
     });
+  }
+
+  void _upworkOauth() async {
+    // define platform (server)
+    var platform = new oauth1.Platform(
+        'https://www.upwork.com/api/auth/v1/oauth/token/request', // temporary credentials request
+        'https://www.upwork.com/services/api/auth', // resource owner authorization
+        'https://www.upwork.com/api/auth/v1/oauth/token/request', // token credentials request
+        oauth1.SignatureMethods.hmacSha1 // signature method
+        );
+
+    // define client credentials (consumer keys)
+    const String apiKey = '';
+    const String apiSecret = '';
+    var clientCredentials = new oauth1.ClientCredentials(apiKey, apiSecret);
+
+    // create Authorization object with client credentials and platform definition
+    var auth = new oauth1.Authorization(clientCredentials, platform);
+
+    // request temporary credentials (request tokens)
+    var res = await auth.requestTemporaryCredentials();
+
+    // redirect to authorization page
+    var url = auth.getResourceOwnerAuthorizationURI(res.credentials.token);
+    print("Open with your browser: $url");
+
+    // request token credentials (access tokens)
+    // return auth.requestTokenCredentials(res.credentials, verifier);
+
+    //   // yeah, you got token credentials
+    //   // create Client object
+    //   var client = new oauth1.Client(
+    //       platform.signatureMethod, clientCredentials, res.credentials);
+
+    //   // now you can access to protected resources via client
+    //   client.get('https://www.upwork.com/api/auth/v1/info.json').then((res) {
+    //     print(res.body);
+    //   });
+
+    //   // NOTE: you can get optional values from AuthorizationResponse object
+    //   print("Res: " + res.toString());
+    // });
   }
 }
